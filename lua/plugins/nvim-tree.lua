ed726ae4dae37c0cb16703fc4424d9304bb602fc
fecar <fecar95@gmail.com>
2025-04-16 20:05:19 -0300

    feat: Add new plugin - ToggleTerm

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,6 @@ return {
   },
   config = function()
     require("nvim-tree").setup({})
-    vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {})
   end,
 }
