efed9c38618d50bbd2e30c181c46cfa49f389e53
Felipe Caetano <fecae95@gmail.com>
2026-04-13 22:24:31 -0300

    add: New configs #2

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        no_italic = false,
-        no_bold = true,
-        no_underline = true,
-      })
-      -- vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				no_italic = false,
+				no_bold = true,
+				no_underline = true,
+			})
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
